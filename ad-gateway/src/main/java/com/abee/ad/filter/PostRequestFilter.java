8faf4a3c19d3f363ea32eb8f4e033bb772441d95
abee <yaoxincong@outlook.com>
2020-05-26 16:18:28 +0800

    Add Zuul Gateway.

---
+++ ad-gateway/src/main/java/com/abee/ad/filter/PostRequestFilter.java
@@ -0,0 +1,48 @@
+package com.abee.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xincong yao
+ */
+@Slf4j
+@Component
+public class PostRequestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        String url = request.getRequestURI();
+        Long startTime = (Long) context.get("startTime");
+        Long endTime = System.currentTimeMillis();
+        Long duration = endTime - startTime;
+
+        log.info("url: " + url + ", duration: " + duration / 100 + "ms");
+
+        return null;
+    }
+}
